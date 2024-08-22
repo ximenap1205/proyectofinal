7326575689c47d67a80005d2a13e660272678443
ximenap1205 <xparedes40@gmail.com>
2024-08-22 20:57:52 -0300

    index link

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('boton').addEventListener('click', function(event) {
     let contra = document.getElementById('contra').value;
 
     if (user && contra) {
-        location.replace("http://127.0.0.1:5500/workspace-inicial/index.html");
+        location.replace("http://127.0.0.1:5500/index.html");
     } else {
         alert('Please fill in the fields.');
     }
