87fe367cd823d4972b0b50e9d33fa52ae3a779cb
lomerrod <lomerrod@gmail.com>
2024-08-22 21:30:32 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -8,12 +8,12 @@ getJSONData(PRODUCTS).then(function(res){
     for (let i = 0; i < productos.length; i++) {
         const producto = productos[i];
         container.innerHTML += `
-    <div class="card">
+    <div class="card-autos">
         <img src="${producto.image}" class="img-card" alt="${producto.name}">
-        <div class="card-body">
-            <h5 class="card-title">${producto.name}</h5>
-            <p class="card-text">${producto.description}</p>
-            <h5 class="price">USD: ${producto.cost}</h5>
+        <div class="card-body-autos">
+            <h5 class="card-title-autos">${producto.name}</h5>
+            <p class="card-text-autos">${producto.description}</p>
+            <h5 class="price-autos">USD: ${producto.cost}</h5>
             <small class="text-muted">Cantidad vendidos: ${producto.soldCount}</small>
         </div>
       </div>
