21d2edf235b9af918e339f14e45dda7e93f5dd5f
ximenap1205 <xparedes40@gmail.com>
2024-08-24 13:34:29 -0300

    login fix

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('boton').addEventListener('click', function(event) {
     let contra = document.getElementById('contra').value;
 
     if (user && contra) {
-        location.replace(location.origin +"/index.html");
+        location.replace(location.origin +"/proyectofinal/index.html");
     } else {
         alert('Please fill in the fields.');
     }
