ecb261b6e5534b99f2da6d330796d99c823d817c
Vanessa Rinaldi <vane.rinaldif4@gmail.com>
2024-10-17 18:20:55 -0300

    desafiate

--- js/my-profile.js
+++ js/my-profile.js
@@ -42,3 +42,21 @@ const nombre = localStorage.getItem('nombre');
   localStorage.setItem('apellido', apellido);
   localStorage.setItem('segundoApellido', document.getElementById('segundo-apellido').value);
   localStorage.setItem('telefono', document.getElementById('telefono').value);
+
+  //Desafiate
+  const defaultFile = 'https://via.placeholder.com/150';
+  const file = document.getElementById('foto');
+  const img = document.getElementById('img');
+  file.addEventListener('change', e => {
+    if (e.target.files[0]){
+      const reader = new FileReader();
+     reader.onload= function (e){
+      img.src = e.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }else {
+      img.src = defaultFile
+    }
+    
+  });
+  localStorage.setItem('foto', document.getElementById('foto').value);
