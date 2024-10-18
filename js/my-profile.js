1deeb1f26fedd14de04e89ec250bfdb879696d7a
lomerrod <lomerrod@gmail.com>
2024-10-17 22:37:50 -0300

    img perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -26,6 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('apellido').value = localStorage.getItem('apellido') || '';
       document.getElementById('segundo-apellido').value = localStorage.getItem('segundoApellido') || '';
       document.getElementById('telefono').value = localStorage.getItem('telefono') || '';
+      
+      // Recupera la foto de localStorage
+      const savedImage = localStorage.getItem('foto');
+      const img = document.getElementById('img');
+      const defaultFile = "../img/img_perfil.png";
+      img.src = savedImage ? savedImage : defaultFile;
+
       const modoDiaNoche = localStorage.getItem('modoDiaNoche');
       if (modoDiaNoche === "noche") document.getElementById('modo-dia-noche').checked = true;
   }
@@ -47,24 +54,39 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('segundoApellido', document.getElementById('segundo-apellido').value);
       localStorage.setItem('telefono', document.getElementById('telefono').value);
       localStorage.setItem('email', email);
+      localStorage.setItem('foto', document.getElementById('foto').value);
   });
 
   // Desafiate
-  const defaultFile = 'https://via.placeholder.com/150';
+  const defaultFile = "../img/img_perfil.png";
   const file = document.getElementById('foto');
   const img = document.getElementById('img');
 
+  //cargar imagen almacenada en ls
+
+  const savedImage = localStorage.getItem('foto');
+    if (savedImage) {
+    img.src = savedImage;
+    } else {
+  img.src = defaultFile;
+}
+  //cambiar imagen
+
   file.addEventListener('change', e => {
-      if (e.target.files[0]) {
+    const file = e.target.files[0];
+      if (file) {
           const reader = new FileReader();
           reader.onload = function (e) {
-              img.src = e.target.result;
+            const imageData = e.target.result;
+            img.src = imageData;
+            localStorage.setItem('foto', imageData);
           }
-          reader.readAsDataURL(e.target.files[0]);
+          reader.readAsDataURL(file);
       } else {
           img.src = defaultFile;
+          localStorage.removeItem('foto');
       }
   });
 
-  localStorage.setItem('foto', document.getElementById('foto').value);
+  
 });
