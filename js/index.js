6b1cac2435de20d31b07d7ce84fe34a476ac3146
lomerrod <lomerrod@gmail.com>
2024-10-27 16:02:23 -0300

    mejorar el guardado a ls de las categorias
    estaba dando error en la consola, solo le agregué ifs a lo que ya estaba

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Manejo de categorías
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html";
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html";
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html";
-    });
+    const autos = document.getElementById("autos");
+    const juguetes = document.getElementById("juguetes");
+    const muebles = document.getElementById("muebles");
 
+    if (autos) {
+        autos.addEventListener("click", function() {
+            localStorage.setItem("catID", 101);
+            window.location = "products.html";
+        });
+    }
+
+    if (juguetes) {
+        juguetes.addEventListener("click", function() {
+            localStorage.setItem("catID", 102);
+            window.location = "products.html";
+        });
+    }
+
+    if (muebles) {
+        muebles.addEventListener("click", function() {
+            localStorage.setItem("catID", 103);
+            window.location = "products.html";
+        });
+    }
 });
-
