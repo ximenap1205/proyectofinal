fdfd813bfc2dcb8c1090e7f5550db7e583c31839
ximenap1205 <xparedes40@gmail.com>
2024-11-27 16:05:52 -0300

    new update

--- js/init.js
+++ js/init.js
@@ -1,11 +1,12 @@
-const CATEGORIES_URL = "http://localhost:3000/cat";
-const PUBLISH_PRODUCT_URL = "http://localhost:3000/publish";
-const PRODUCTS_URL = "http://localhost:3000/products/";
-const PRODUCT_INFO_URL = "http://localhost:3000/product_info/";
-const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/product_comments/";
-const CART_INFO_URL = "http://localhost:3000/cart_info/";
-const CART_BUY_URL = "http://localhost:3000/cart_buy";
-const EXT_TYPE = "http://localhost:3000/ext_type";
+const CATEGORIES_URL = "http://localhost:3000/categories"; 
+const PUBLISH_PRODUCT_URL = "http://localhost:3000/publish_products"; 
+const PRODUCTS_URL = "http://localhost:3000/products/"; 
+const PRODUCT_INFO_URL = "http://localhost:3000/product_info/"; 
+const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/products_comments/"; 
+const CART_INFO_URL = "http://localhost:3000/user_cart"; 
+const CART_BUY_URL = "http://localhost:3000/cart"; 
+const EXT_TYPE = "http://localhost:3000/ext_type"; 
+
 
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
