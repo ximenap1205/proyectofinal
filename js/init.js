d601e4ff58d5e3e4b7e7d902448d9230254236c5
ximenap1205 <xparedes40@gmail.com>
2024-11-28 14:30:05 -0300

    Revert "Merge pull request #15 from ximenap1205/ENTREGA-8"
    This reverts commit 4110eab7a2de58fc5ccbe8a3ba2f784a3aa35b6f, reversing
    changes made to 1dbd1a6af1335cc53c668f185f1a17713d438bc4.

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
-const CATEGORIES_URL = "http://localhost:3000/cat";
-const PUBLISH_PRODUCT_URL = "http://localhost:3000/publish";
-const PRODUCTS_URL = "http://localhost:3000/products/";
-const PRODUCT_INFO_URL = "http://localhost:3000/product_info/";
-const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/product_comments/";
-const CART_INFO_URL = "http://localhost:3000/cart_info/";
-const CART_BUY_URL = "http://localhost:3000/cart_buy";
-const EXT_TYPE = "http://localhost:3000/ext_type";
+const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
+const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
+const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
+const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
+const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
+const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
+const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+const EXT_TYPE = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
@@ -39,39 +39,3 @@ let getJSONData = function(url){
         return result;
     });
 }
-
-//Productos dinamicos
-let getProductData = function(productId) {
-  return fetch(`http://localhost:3000/products/${productId}`)
-      .then(response => {
-          if (response.ok) {
-              return response.json();
-          } else {
-              throw Error("Producto no encontrado");
-          }
-      })
-      .then(data => {
-          console.log(data); // Aca se puede procesar la información del producto
-      })
-      .catch(error => {
-          console.error(error);
-      });
-};
-
-//Comentarios dinamicos
-let getCommentsData = function(productId) {
-  return fetch(`http://localhost:3000/products_comments/${productId}`)
-      .then(response => {
-          if (response.ok) {
-              return response.json();
-          } else {
-              throw Error("Comentarios no encontrados");
-          }
-      })
-      .then(data => {
-          console.log(data);  // Aca se puede procesar la información del producto
-      })
-      .catch(error => {
-          console.error(error);
-      });
-};
