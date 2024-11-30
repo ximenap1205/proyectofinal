f2ec00879304a5fe9ea420c94dfe628c6390e490
Vanessa Rinaldi <vane.rinaldif4@gmail.com>
2024-11-30 14:25:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -60,16 +60,6 @@ app.post('/login', (req, res) => {
 });
 
 
-
-// Verificar conexión
-mariadb.connect(err => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err);
-    } else {
-        console.log('Conectado a la base de datos.');
-    }
-});
-
 app.post('/cart', async (req, res) => {
     const { user_id, items } = req.body;
 
